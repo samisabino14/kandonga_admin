65a831cf2c89eb854efac5c26f5cb7ef5d596b72
Sami Armando Muongueno Sabino <67502803+samisabino14@users.noreply.github.com>
2022-11-01 20:37:08 +0100

    Update config.js

--- kandonga/config.js
+++ kandonga/config.js
@@ -2,14 +2,14 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDC56nvblQicHcZop52iEu5fszShoSKbpk",
-    authDomain: "kandonga77-bbfd7.firebaseapp.com",
-    databaseURL: "https://kandonga77-bbfd7-default-rtdb.firebaseio.com",
-    projectId: "kandonga77-bbfd7",
-    storageBucket: "kandonga77-bbfd7.appspot.com",
-    messagingSenderId: "421519571088",
-    appId: "1:421519571088:web:066fdb1df82f264bf14015",
-    measurementId: "G-6ZVM4W45EV"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "https://",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 
